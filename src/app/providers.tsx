1e2d868545e12bf616c1c10d9ae89af6537d311a
Sayan Biswas <98510117+sayanbiswascoder@users.noreply.github.com>
2026-02-16 03:43:55 +0530

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { ThirdwebProvider } from "thirdweb/react";
-import { client } from "./client";
-import { sepolia } from "thirdweb/chains";
 
 export default function Providers({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <ThirdwebProvider client={client} activeChain={sepolia}>
-      {children}
-    </ThirdwebProvider>
-  );
+  return <ThirdwebProvider>{children}</ThirdwebProvider>;
 }
